9158de17256a6a1dc942903bb9ccfcf08dcc586f
Evgeniy Leutskiy <eleutskiy@ptsecurity.ru>
2018-05-15 00:34:51 +0700

    create the Login window and start to do the logical part with the VK.NET assembly

---
+++ VK/GlobalSettings.cs
@@ -0,0 +1,25 @@
+﻿namespace VK
+{
+    /// <summary>
+    /// Настройки приложения
+    /// </summary>
+    public static class GlobalSettings
+    {
+        /// <summary>
+        /// Идентификатор приложения
+        /// </summary>
+        public static string AppId { get; set; }
+
+        /// <summary>
+        /// Номер телефона или email
+        /// </summary>
+        public static string Login => @"dzhon.argentum@mail.ru";
+
+        /// <summary>
+        /// Пароль
+        /// </summary>
+        public static string Password => @"WESTall89";
+
+        public static string Token { get; set; }
+    }
+}
---
+++ VK/SignInWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using VkNet;
+using VkNet.Enums.Filters;
+using MessageBox = System.Windows.MessageBox;
+
+namespace VK
+{
+    /// <summary>
+    /// Interaction logic for SignInWindow.xaml
+    /// </summary>
+    public partial class SignInWindow : Window
+    {
+        private readonly VkApi _api = new VkApi();
+
+        public SignInWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SignIn_Click(object sender, RoutedEventArgs e)
+        {
+            var appId = ConfigurationManager.AppSettings["appId"];
+
+            GlobalSettings.AppId = appId;
+
+            var login = accountBox.Text;
+            var password = passwordBox.Password;
+
+            _api.Authorize(new ApiAuthParams
+            {
+                ApplicationId = ulong.Parse(appId),
+                Login = login,
+                Password = password,
+                Settings = Settings.All
+            });
+
+            //TODO: make the incorrect scenario with the red highlight
+            if (string.IsNullOrEmpty(_api.Token))
+            {
+                accountBox.Text = string.Empty;
+                passwordBox.Password = string.Empty;
+
+                return;
+            }
+
+            GlobalSettings.Token = _api.Token;
+
+            var mainWindow = new MainWindow();
+
+            this.Close();
+            mainWindow.ShowDialog();
+        }
+    }
+}
